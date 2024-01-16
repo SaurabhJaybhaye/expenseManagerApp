e0ab7596d2f41c72d688efbcf520de4097d001fd
Saurabh <saurabhjaybhaye01@gmail.com>
2024-01-16 20:34:55 +0530

    updated the navigation

--- src/navigation/AppNavigator.jsx
+++ src/navigation/AppNavigator.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
-import { createStackNavigator } from "@react-navigation/stack";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { PATHS } from "../shared/Constants";
 import DrawerNavigator from "./DrawerNavigator";
 import FormsScreen from "../screens/FormsScreen";
 
-const Stack = createStackNavigator();
+const Stack = createNativeStackNavigator();
 
 const AppNavigator = () => {
   return (
